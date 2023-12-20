4f37cb953d7a27ad8a405e43d7040a0158721876
nafise <nafise.faridi@gmail.com>
2023-12-19 22:45:39 -0800

    adding print function
    and deleting unnecessary files

--- printMenu.cpp
+++ printMenu.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
+//function for menu
+void print();
+//function for add
+//function for mean
+//function for max
+//function for min
+//function for print
+//function for 
+
 
 int main(){
-	cout<<"Hello world"<<endl;
+	print();
 	return 0;
+}
+
+void print(){
+	cout<<"Choose one of the following options"<<endl;
+	cout<<"A: add a data"<<endl;
+	cout<<"P: print the data"<<endl;
+	cout<<"L: show the largest data"<<endl;
+	cout<<"S: show the smallest data"<<endl;
+	cout<<"Q: quit the program"<<endl;
 }
