afa7f6b01445fc606ed13b873fc87cd60cd9b8e5
nafise <nafise.faridi@gmail.com>
2023-12-20 19:29:35 -0800

    adding all the functions and update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # programmingCpp
+This project is defined to work with functions and passing by the reference.
+
+The user is interacting with a menu. They can add a number to an array and 
+take the mean and max of the new array.
--- printMenu.cpp
+++ printMenu.cpp
@@ -7,30 +7,33 @@ void print_menu();
 //function for add
 void add_data(vector<int>&);
 //function for mean
+void mean_of_data(vector<int>&);
 //function for max
-
-//function for min
+void max_of_data(vector<int>);
 //function for print
 void print(const vector<int>&);
 //function for 
 vector <int> arr;
 
 int main(){
-	char user_input;
+	char user_input{};
 	do{
 		print_menu();
 		cin >> user_input;
-		if(user_input=='m'||user_input=='M')
-			print_menu();
-		else if(user_input=='p'||user_input=='P')
+		user_input=toupper(user_input);
+		if(user_input=='P')
 			print(arr);
-		else if(user_input=='a'||user_input=='A')
+		else if(user_input=='A')
 			add_data(arr);
-		else if(user_input=='q'||user_input=='Q')
+		else if(user_input=='Q')
 			cout<<"thank you for interacting, see you again!"<<endl;
+		else if(user_input=='L')
+			max_of_data(arr);
+		else if(user_input=='M')
+			mean_of_data(arr);
 		else
 			cout<<"onacceptable input"<<endl;
-	}while(user_input!='q'&& user_input!='Q');
+	}while(user_input!='Q');
 	return 0;
 }
 void add_data(vector<int> &arr){
@@ -46,17 +49,41 @@ void print_menu(){
 	cout<<"A: add a data"<<endl;
 	cout<<"P: print the data"<<endl;
 	cout<<"L: show the largest data"<<endl;
-	cout<<"S: show the smallest data"<<endl;
+	cout<<"M: show the mean of data"<<endl;
 	cout<<"Q: quit the program"<<endl;
 }
 void print(const vector<int> &arr){
 	int size=arr.size();
-	if (size<1)
+	if (arr.empty())
 		cout<<"[], the list is empty"<<endl;
 	else{
 		cout<<"[";
 		for (int i{0};i<size;i++)
 		cout<<" "<<arr[i];
 		cout<<" ]"<<endl;
+	}
+}
+void max_of_data(vector<int> arr){
+	if (arr.empty())
+		cout<<"no Array, please add numbers first"<<endl;
+	else{
+	int max_value=arr[0];
+	for(auto i:arr){
+		if(i>max_value)
+		max_value=i;
+	}
+	cout<<"the largest value is: "<<max_value<<endl;
+	}
+}
+void mean_of_data(vector<int> &arr){
+	if (arr.empty())
+		cout<<"no Array, please add numbers first"<<endl;
+	else{
+	int total_value{};
+	for(auto i:arr){
+		total_value+=i;
+	}
+	
+	cout<<"the mean value is: "<<static_cast<double> (total_value)/arr.size()<<endl;
 	}
 }
